566f989112eb2f622dbc5bd47b81fe659b69f6ff
Henishnk2508 <henishnk@gmail.com>
2024-03-17 16:08:43 +0530

    Updates

---
+++ precache-manifest.f4e5fe0b99755317d80e316bef31d301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa685f162b665ce7dfc7de16c5ef5da",
+    "url": "/Textutils_ReactApp/index.html"
+  },
+  {
+    "revision": "0d078f11467ad5d5a6aa",
+    "url": "/Textutils_ReactApp/static/css/main.06878d6d.chunk.css"
+  },
+  {
+    "revision": "ae05e3cd37358f0c71a4",
+    "url": "/Textutils_ReactApp/static/js/2.9980a0c2.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Textutils_ReactApp/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "0d078f11467ad5d5a6aa",
+    "url": "/Textutils_ReactApp/static/js/main.5f2a671d.chunk.js"
+  },
+  {
+    "revision": "922935539385cffeb884",
+    "url": "/Textutils_ReactApp/static/js/runtime~main.442ebce0.js"
+  }
+]);
